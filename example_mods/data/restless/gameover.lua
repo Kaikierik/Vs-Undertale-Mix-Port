d403122e458e6f78ad592584c3ee7b8cd58755f5
Kaikierik <111656679+Kaikierik@users.noreply.github.com>
2022-12-15 21:16:30 -0200

    Add files via upload

---
+++ example_mods/data/restless/gameover.lua
@@ -0,0 +1,6 @@
+function onCreate()
+    setPropertyFromClass('GameOverSubstate', 'characterName', 'frisk');
+    setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'die')
+    setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver');
+    setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd');
+end
